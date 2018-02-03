248eb697bf562ac56a086176808d56a8d5ddf6fb
Mark Raynsford <code@io7m.com>
2018-02-03 14:44:17 +0000

    Expand the example code to download artifacts

--- src/main/java/com/io7m/resolver/Main.java
+++ src/main/java/com/io7m/resolver/Main.java
@@ -10,20 +10,31 @@ import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
 import org.eclipse.aether.impl.DefaultServiceLocator;
 import org.eclipse.aether.repository.LocalRepository;
 import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResult;
 import org.eclipse.aether.resolution.VersionRangeRequest;
 import org.eclipse.aether.resolution.VersionRangeResult;
 import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
 import org.eclipse.aether.spi.connector.transport.TransporterFactory;
+import org.eclipse.aether.transfer.TransferEvent;
+import org.eclipse.aether.transfer.TransferListener;
 import org.eclipse.aether.transport.http.HttpTransporterFactory;
 import org.eclipse.aether.version.Version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 
 public final class Main
 {
   private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+  public static final String TMP_REPO = "/tmp/repo";
 
   private Main()
   {
@@ -37,20 +48,78 @@ public final class Main
     final RepositorySystem system = newRepositorySystem();
     final RepositorySystemSession session = newRepositorySystemSession(system);
 
-    final Artifact artifact =
-      new DefaultArtifact(
-        "com.io7m.junreachable:com.io7m.junreachable:[2.0.0,)");
+    {
+      final Path path = Paths.get(TMP_REPO);
+      Files.walk(path, FileVisitOption.FOLLOW_LINKS)
+        .sorted(Comparator.reverseOrder())
+        .map(Path::toFile)
+        .peek(System.out::println)
+        .forEach(File::delete);
+    }
 
-    final VersionRangeRequest request = new VersionRangeRequest();
-    request.setArtifact(artifact);
-    request.setRepositories(newRepositories());
+    /*
+     * It's necessary to construct the artifact string manually, because
+     * the Maven Shade plugin will mangle the string if it is specified
+     * as a constant.
+     *
+     * See https://issues.apache.org/jira/browse/MSHADE-156
+     */
 
-    final VersionRangeResult result =
-      system.resolveVersionRange(session, request);
-    final Version newest =
-      result.getHighestVersion();
+    final String initial_range =
+      new StringBuilder(128)
+        .append("com")
+        .append(".google")
+        .append(".guava")
+        .append(":")
+        .append("guava")
+        .append(":")
+        .append("[23.0,)")
+        .toString();
 
-    LOG.debug("version: {}", newest);
+    final Version version;
+
+    {
+      final Artifact artifact =
+        new DefaultArtifact(initial_range);
+
+      final VersionRangeRequest request = new VersionRangeRequest();
+      request.setArtifact(artifact);
+      request.setRepositories(newRepositories());
+
+      final VersionRangeResult result =
+        system.resolveVersionRange(session, request);
+      version = result.getHighestVersion();
+      LOG.debug("version: {}", version);
+    }
+
+    {
+      /*
+       * It's necessary to construct the artifact string manually, because
+       * the Maven Shade plugin will mangle the string if it is specified
+       * as a constant.
+       *
+       * See https://issues.apache.org/jira/browse/MSHADE-156
+       */
+
+      final String current_artifact =
+        new StringBuilder(128)
+          .append("com")
+          .append(".google")
+          .append(".guava")
+          .append(":")
+          .append("guava")
+          .append(":")
+          .append(version)
+          .toString();
+
+      final Artifact artifact = new DefaultArtifact(current_artifact);
+      final ArtifactRequest request = new ArtifactRequest();
+      request.setArtifact(artifact);
+      request.setRepositories(newRepositories());
+
+      final ArtifactResult result = system.resolveArtifact(session, request);
+      LOG.debug("result: {}", result.getArtifact().getFile());
+    }
   }
 
   private static List<RemoteRepository> newRepositories()
@@ -73,11 +142,12 @@ public final class Main
     final DefaultRepositorySystemSession session =
       MavenRepositorySystemUtils.newSession();
     final LocalRepository local_repos =
-      new LocalRepository("/tmp/repo");
+      new LocalRepository(TMP_REPO);
 
     session.setLocalRepositoryManager(
       system.newLocalRepositoryManager(session, local_repos));
 
+    session.setTransferListener(new LoggingTransferListener());
     return session;
   }
 
@@ -107,5 +177,53 @@ public final class Main
   private static DefaultServiceLocator newServiceLocator()
   {
     return MavenRepositorySystemUtils.newServiceLocator();
+  }
+
+  private static class LoggingTransferListener implements TransferListener
+  {
+    @Override
+    public void transferInitiated(final TransferEvent event)
+    {
+      LOG.debug("transferInitiated: {}", event);
+    }
+
+    @Override
+    public void transferStarted(final TransferEvent event)
+    {
+      LOG.debug("transferStarted: {}", event);
+    }
+
+    @Override
+    public void transferProgressed(final TransferEvent event)
+    {
+      LOG.debug("transferProgressed: {}", event);
+
+      final long received = event.getTransferredBytes();
+      final long expected = event.getResource().getContentLength();
+      final double percent = (((double) received) / ((double) expected)) * 100.0;
+
+      LOG.debug("progress: {}/{} ({}%)",
+                Long.valueOf(received),
+                Long.valueOf(expected),
+                String.format("%.2f", Double.valueOf(percent)));
+    }
+
+    @Override
+    public void transferCorrupted(final TransferEvent event)
+    {
+      LOG.debug("transferCorrupted: {}", event);
+    }
+
+    @Override
+    public void transferSucceeded(final TransferEvent event)
+    {
+      LOG.debug("transferSucceeded: {}", event);
+    }
+
+    @Override
+    public void transferFailed(final TransferEvent event)
+    {
+      LOG.debug("transferFailed: {}", event);
+    }
   }
 }
